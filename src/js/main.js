263c001bbb93dbb7b4b7aa2f8e719e0593acfafd
Kamil Preweda <preweda.kamil@o2.pl>
2019-07-24 16:41:09 +0200

    added scrub function

--- src/js/main.js
+++ src/js/main.js
@@ -20,7 +20,7 @@ if ('serviceWorker' in navigator) {
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress_filled');
+const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
@@ -46,8 +46,13 @@ function handleRangeUpdate() {
 }
 
 function handleProgress() {
-  const percent = (vide.currentTime / video.duration) * 100;
+  const percent = (video.currentTime / video.duration) * 100;
   progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
 }
 //Hook up the elements listeners //
 video.addEventListener('click', togglePlay);
@@ -57,3 +62,4 @@ video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+progress.addEventListener('click', scrub);
