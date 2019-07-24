7e3a234096da983fb966f13b275076706f176f8e
Kamil Preweda <preweda.kamil@o2.pl>
2019-07-24 16:35:25 +0200

    added handleProgress function

--- src/js/main.js
+++ src/js/main.js
@@ -45,10 +45,15 @@ function handleRangeUpdate() {
   video[this.name] = this.value;
 }
 
+function handleProgress() {
+  const percent = (vide.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
 //Hook up the elements listeners //
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
